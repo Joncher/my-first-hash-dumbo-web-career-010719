a79fafba0243b46a6f9e5af4969c7fa325c3cea7
John Crespi Oncher <johnoncher@gmail.com>
2018-12-23 22:00:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,8 +30,10 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
+  shipping_manifest[muskets] => 2
 
 
-  # return the shipping_manifest hash below
+  shipping_manifest[gun powder] => 4
+
   return shipping_manifest
 end
